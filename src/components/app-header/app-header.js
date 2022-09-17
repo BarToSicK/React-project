00abbeedffb510ad7d811a40594253d3b988bc8f
BarToSick <nikitospetrenko51@gmail.com>
2022-09-17 14:59:21 +0300

    React 3

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+import './app-header.css';
+
 const AppHeader = () => {
     return (
-        <div className={"app-header d-flex"}>
+        <div className={'app-header d-flex'}>
             <h1>Nikita Petrenko</h1>
             <h2>5 записей, из них понравлось 0</h2>
         </div>
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
@@ -6,34 +6,64 @@ import PostStatusFilter from "../post-status-filter";
 import PostList from "../post-list";
 import PostAddForm from "../post-add-form";
 
-import "./app.css";
-import "../app-header/app-header.css"
-import "../post-add-form/post-add-form.css"
-import "../post-list/post-list.css"
-import "../post-list-item/post-list-item.css"
-import "../post-status-filter/post-status-filter.css"
-import "../search-panel/search-panel.css"
+import './app.css';
 
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                {label: 'Going to learn React', important: true, id: 1},
+                {label: 'That is so good', important: false, id: 2},
+                {label: 'I need a break...', important: false, id: 3}
+            ]
+        };
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
 
-const App = () => {
+        this.maxId = 4;
+    }
 
-    const data = [
-        {label: 'Going to learn React', important: true, id: 'sds'},
-        {label: 'That is so good', important: false, id: 'asd'},
-        {label: 'I need a break...', important: false, id: 'fdd'},
-    ];
+    deleteItem(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
 
-    return (
-        <div className={"app"}>
-            <AppHeader/>
-            <div className={"search-panel d-flex"}>
-                <SearchPanel/>
-                <PostStatusFilter/>
+            const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
+
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className={'app'}>
+                <AppHeader/>
+                <div className={"search-panel d-flex"}>
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList
+                    posts={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <PostAddForm
+                  onAdd={this.addItem}/>
             </div>
-            <PostList posts={data}/>
-            <PostAddForm/>
-        </div>
-    )
+        )
+    }
 }
-
-export default App;
